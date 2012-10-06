f687700c92e769802787f9c0e927d8652eb06524
Ryo NAKAMURA <ryo-nakamura@cookpad.com>
2012-10-07 03:44:20 +0900

    Iremocon can connect to tcp server

--- lib/iremocon.rb
+++ lib/iremocon.rb
@@ -1,4 +1,22 @@
 require "iremocon/version"
+require "net/telnet"
 
 class Iremocon
+  attr_reader :host, :port, :telnet
+
+  def initialize(host, port)
+    @host = host
+    @port = port
+    connect
+  end
+
+  private
+
+  def connect
+    @telnet = Net::Telnet.new("Host" => @host, "Port" => @port)
+  rescue Errno::ECONNREFUSED
+    raise ConnectionError.new("Connection failed - #{@host}:#{@port}")
+  end
+
+  class ConnectionError < StandardError; end
 end
--- spec/iremocon_spec.rb
+++ spec/iremocon_spec.rb
@@ -2,10 +2,22 @@ require "spec_helper"
 
 describe Iremocon do
   describe "#new" do
-    it do
-      expect do
-        Iremocon.new
-      end.to_not raise_error
+    context "when connection succeeded" do
+      it "should connect to tcp server with given host and port" do
+        server = TCPServer.new("127.0.0.1", 50000)
+        client = Iremocon.new("127.0.0.1", 50000)
+        client.telnet.should be_a(Net::Telnet)
+        client.telnet.close
+        server.close
+      end
+    end
+
+    context "when connection failed" do
+      it do
+        expect do
+          Iremocon.new("127.0.0.1", 60000)
+        end.to raise_error(Iremocon::ConnectionError)
+      end
     end
   end
 end
