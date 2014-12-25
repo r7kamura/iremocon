ae8e1c7dc4533add518ac3aa6fd8214f8dd817ba
morikat <morika-t@outlook.com>
2014-12-25 19:48:16 +0900

    Fix Deprecation Warning

--- spec/iremocon_spec.rb
+++ spec/iremocon_spec.rb
@@ -10,7 +10,7 @@ describe Iremocon do
   end
 
   before do
-    client.stub(:puts) # To silence a command result
+    allow(client).to receive(:puts) # To silence a command result
   end
 
   after do
@@ -21,7 +21,7 @@ describe Iremocon do
   describe "#new" do
     context "when connection succeeded" do
       it "should connect to tcp server by Net::Telnet client" do
-        client.telnet.should be_a(Net::Telnet)
+        expect(client.telnet).to be_kind_of(Net::Telnet)
       end
     end
 
@@ -36,85 +36,85 @@ describe Iremocon do
 
   describe "#au" do
     it "should send *au" do
-      client.telnet.should_receive(:cmd).with("*au")
+      expect(client.telnet).to receive(:cmd).with("*au")
       client.au
     end
   end
 
   describe "#is" do
     it "should send *is;<channel>" do
-      client.telnet.should_receive(:cmd).with("*is;1")
+      expect(client.telnet).to receive(:cmd).with("*is;1")
       client.is(1)
     end
   end
 
   describe "#ic" do
     it "should send *ic;<channel>" do
-      client.telnet.should_receive(:cmd).with("*ic;1")
+      expect(client.telnet).to receive(:cmd).with("*ic;1")
       client.ic(1)
     end
   end
 
   describe "#cc" do
     it "should send *cc" do
-      client.telnet.should_receive(:cmd).with("*cc")
+      expect(client.telnet).to receive(:cmd).with("*cc")
       client.cc
     end
   end
 
   describe "#tm" do
     it "should send *tm;<channel>;<time>;<interval>" do
-      client.telnet.should_receive(:cmd).with("*tm;1;946652400;60")
+      expect(client.telnet).to receive(:cmd).with("*tm;1;946652400;60")
       client.tm(1, 946652400, 60)
     end
 
     it "should convert time to epoch time" do
-      client.telnet.should_receive(:cmd).with("*tm;1;946652400;60")
+      expect(client.telnet).to receive(:cmd).with("*tm;1;946652400;60")
       client.tm(1, Time.local(2000), 60)
     end
 
     it "should complement interval with 0 as default" do
-      client.telnet.should_receive(:cmd).with("*tm;1;946652400;0")
+      expect(client.telnet).to receive(:cmd).with("*tm;1;946652400;0")
       client.tm(1, Time.local(2000))
     end
   end
 
   describe "#tl" do
     it "should send *tl" do
-      client.telnet.should_receive(:cmd).with("*tl")
+      expect(client.telnet).to receive(:cmd).with("*tl")
       client.tl
     end
   end
 
   describe "#td" do
     it "should send *td;<timer_id>" do
-      client.telnet.should_receive(:cmd).with("*td;1")
+      expect(client.telnet).to receive(:cmd).with("*td;1")
       client.td(1)
     end
   end
 
   describe "#ts" do
     it "should send *ts;<time>" do
-      client.telnet.should_receive(:cmd).with("*ts;946652400")
+      expect(client.telnet).to receive(:cmd).with("*ts;946652400")
       client.ts(946652400)
     end
 
     it "should convert time to epoch time" do
-      client.telnet.should_receive(:cmd).with("*ts;946652400")
+      expect(client.telnet).to receive(:cmd).with("*ts;946652400")
       client.ts(Time.local(2000))
     end
   end
 
   describe "#tg" do
     it "should send *tg" do
-      client.telnet.should_receive(:cmd).with("*tg")
+      expect(client.telnet).to receive(:cmd).with("*tg")
       client.tg
     end
   end
 
   describe "#vr" do
     it "should send *vr" do
-      client.telnet.should_receive(:cmd).with("*vr")
+      expect(client.telnet).to receive(:cmd).with("*vr")
       client.vr
     end
   end
